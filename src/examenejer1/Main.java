59a1ed9852eac541c540bc30697df343dd5647dc
Irene <irene@hmail.com>
2022-12-14 11:08:12 +0100

    commit irenesifre intento 2

---
+++ src/examenejer1/Main.java
@@ -0,0 +1,77 @@
+package examenejer1;
+import java.util.Scanner;
+public class Main {
+
+  public static void main(String[] args) {
+    
+   Scanner teclado = new Scanner (System.in);
+   
+  
+   int edad;
+   double altura;
+   
+   System.out.println("Introduce la edad y altura de 5 alumnos");
+  
+   for (int i=0; i<5; i ++);
+  
+   System.out.println("Edad alumno1");
+   edad = teclado.nextInt();
+   
+   System.out.println("Altura alumno1");
+   altura = teclado.nextDouble();
+   
+  
+   System.out.println("Edad alumno2");
+   edad = teclado.nextInt();
+   
+   System.out.println("Altura alumno2");
+   altura = teclado.nextDouble();
+   
+   
+   
+   System.out.println("Edad alumno3");
+   edad = teclado.nextInt();
+   
+   System.out.println("Altura alumno3");
+   altura = teclado.nextDouble();
+  
+   
+   
+   System.out.println("Edad alumno4");
+   edad = teclado.nextInt();
+   
+   System.out.println("Altura alumno4");
+   altura = teclado.nextDouble();
+    
+   
+   
+   System.out.println("Edad alumno5");
+   edad = teclado.nextInt();
+   
+   System.out.println("Altura alumno5");
+   altura= teclado.nextDouble();
+  
+   
+   double suma= edad/2;
+   
+   System.out.println("La edad media del alumnado es " + suma + " años ");
+   
+   double sumaltura= altura/2;
+   System.out.println("La altura media del alumnado es de " + sumaltura);
+
+
+  }
+        
+  }
+   
+
+
+ 
+ 
+   
+     
+     
+   
+   
+  
+
